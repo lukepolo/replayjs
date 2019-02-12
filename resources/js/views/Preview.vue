d1d13ef4d7737ce6e80a2b97df6434fbf3a68c2f
Luke Policinski <luke@lukepolo.com>
2019-02-11 20:04:15 -0500

    removing vendor

--- resources/js/views/Preview.vue
+++ resources/js/views/Preview.vue
@@ -1,4 +1,74 @@
 <style>
+#preview {
+  transform: scale(1);
+  transform-origin: 0 0;
+}
+</style>
+<template>
+  <div>
+    <iframe id="preview"></iframe>
+  </div>
+</template>
+<script>
+import Vue from "vue";
+import { TreeMirror } from "tree-mirror";
+export default Vue.extend({
+  $inject: ["BroadcastService"],
+  data() {
+    return {
+      base: null,
+      previewDocument: null,
+      previewFrame: null,
+      clickTimeout: null,
+      initialized: false,
+    };
+  },
+  mounted() {
+    this.previewFrame = document.getElementById("preview");
+    this.previewDocument = document.getElementById(
+      "preview",
+    ).contentWindow.document;
+
+    while (this.previewDocument.firstChild) {
+      this.previewDocument.removeChild(this.previewDocument.firstChild);
+    }
+
+    let mirror = new TreeMirror(this.previewDocument, {
+      createElement: (tagName) => {
+        if (tagName === "SCRIPT") {
+          let node = document.createElement("NO-SCRIPT");
+          node.style.display = "none";
+          return node;
+        }
+        if (tagName === "HEAD") {
+          let node = document.createElement("HEAD");
+          // TODO - this only fixes relative URLS not absolute
+          let baseHref = document.createElement("BASE");
+          node.appendChild(baseHref);
+          node.firstChild.href = "http://codepier.test/";
+          return node;
+        }
+      },
+    });
+    this.channel = this.broadcastService.join(`chat`);
+    this.channel
+      .listenForWhisper("initialize", ({ rootId, children, base }) => {
+        if (!this.initialized) {
+          this.base = base;
+          mirror.initialize(rootId, children);
+
+          let cursorNode = document.createElement("DIV");
+          cursorNode.id = "cursor";
+
+          let clicksNode = document.createElement("DIV");
+          clicksNode.id = "clicks";
+
+          console.info("PUT IN");
+          this.previewDocument.body.appendChild(cursorNode);
+          this.previewDocument.body.appendChild(clicksNode);
+
+          let styles = document.createElement("style");
+          styles.innerHTML = `
 @-webkit-keyframes load-progress {
   100% {
     opacity: 0;
@@ -30,87 +100,16 @@
 
 #cursor {
   top: 1px;
-  z-index: 1;
+  z-index: 999999999999999999;
   width: 25px;
   height: 25px;
   position: absolute;
   background-size: contain;
   background-repeat: no-repeat;
-  background-image: url("./../../images/cursor.png");
+  background-image: url("http://replayjs.test/img/cursor.png");
 }
-
-#preview {
-  transform: scale(1);
-  transform-origin: 0 0;
-}
-</style>
-<template>
-  <div>
-    <iframe id="preview"></iframe>
-  </div>
-</template>
-<script>
-import Vue from "vue";
-import { TreeMirror } from "tree-mirror";
-export default Vue.extend({
-  $inject: ["BroadcastService"],
-  data() {
-    return {
-      base: null,
-      previewDocument: null,
-      previewFrame: null,
-      clickTimeout: null,
-      initialized: false,
-    };
-  },
-  mounted() {
-    this.previewFrame = document.getElementById("preview");
-    this.previewDocument = document.getElementById(
-      "preview",
-    ).contentWindow.document;
-
-    // let cssLink = document.createElement("link");
-    //     cssLink.href = "preview-styles.css";
-    //     cssLink.rel = "stylesheet";
-    //     cssLink.type = "text/css";
-    // this.previewDocument.head.appendChild(cssLink);
-
-    while (this.previewDocument.firstChild) {
-      this.previewDocument.removeChild(this.previewDocument.firstChild);
-    }
-
-    let mirror = new TreeMirror(this.previewDocument, {
-      createElement: (tagName) => {
-        if (tagName === "SCRIPT") {
-          let node = document.createElement("NO-SCRIPT");
-          node.style.display = "none";
-          return node;
-        }
-        if (tagName === "HEAD") {
-          let node = document.createElement("HEAD");
-          // TODO - this only fixes relative URLS not absolute
-          node.appendChild(document.createElement("BASE"));
-          // node.firstChild.href = 'http://localdev/dev/fssa/voucher/';
-          return node;
-        }
-      },
-    });
-    this.channel = this.broadcastService.join(`chat`);
-    this.channel
-      .listenForWhisper("initialize", ({ rootId, children, base }) => {
-        if (!this.initialized) {
-          this.base = base;
-          mirror.initialize(rootId, children);
-
-          let cursorNode = document.createElement("DIV");
-          cursorNode.id = "cursor";
-
-          let clicksNode = document.createElement("DIV");
-          clicksNode.id = "clicks";
-
-          console.info("PUT IN");
-          this.previewDocument.body.appendChild(cursorNode);
-          this.previewDocument.body.appendChild(clicksNode);
+`;
+          this.previewDocument.head.appendChild(styles);
         }
 
         this.initialized = true;
