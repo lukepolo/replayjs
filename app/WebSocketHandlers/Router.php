c29d53183cff7d4d23c73ce7a8aa20ce3a7bcf2c
Luke Policinski <luke.policinski@egov.com>
2019-02-28 16:05:57 -0500

    we are getting close

--- app/WebSocketHandlers/Router.php
+++ app/WebSocketHandlers/Router.php
@@ -3,12 +3,20 @@
 namespace App\WebSocketHandlers;
 
 use App\WebSocketHandlers\WebRecorderHandler;
+use BeyondCode\LaravelWebSockets\HttpApi\Controllers\FetchUsersController;
+use BeyondCode\LaravelWebSockets\HttpApi\Controllers\FetchChannelController;
+use BeyondCode\LaravelWebSockets\HttpApi\Controllers\TriggerEventController;
+use BeyondCode\LaravelWebSockets\HttpApi\Controllers\FetchChannelsController;
 
 class Router extends \BeyondCode\LaravelWebSockets\Server\Router
 {
-    public function __construct()
-      {
-          parent::__construct();
-          $this->webSocket('/web-recorder', WebRecorderHandler::class);
-     }
+    public function echo()
+    {
+        $this->get('/app/{appKey}', WebRecorderHandler::class);
+
+        $this->post('/apps/{appId}/events', TriggerEventController::class);
+        $this->get('/apps/{appId}/channels', FetchChannelsController::class);
+        $this->get('/apps/{appId}/channels/{channelName}', FetchChannelController::class);
+        $this->get('/apps/{appId}/channels/{channelName}/users', FetchUsersController::class);
+    }
 }
--- app/WebSocketHandlers/WebRecorderHandler.php
+++ app/WebSocketHandlers/WebRecorderHandler.php
@@ -4,11 +4,13 @@ namespace App\WebSocketHandlers;
 
 use Ratchet\ConnectionInterface;
 use Ratchet\RFC6455\Messaging\MessageInterface;
+use BeyondCode\LaravelWebSockets\WebSockets\WebSocketHandler;
 
-class WebRecorderHandler extends \BeyondCode\LaravelWebSockets\WebSockets\WebSocketHandler
+class WebRecorderHandler extends WebSocketHandler
 {
     public function onMessage(ConnectionInterface $connection, MessageInterface $message)
     {
-
+        dump(json_parse($message-getPayload()));
+        parent::onMessage($connection, $message);
     }
 }
