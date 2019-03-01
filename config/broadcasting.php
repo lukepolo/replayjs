f55505328360c637a760a099aebb99a392362ff3
Luke Policinski <luke.policinski@egov.com>
2019-03-01 12:40:08 -0500

    fixing wrong port number

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -39,7 +39,7 @@ return [
                 'cluster' => env('PUSHER_APP_CLUSTER'),
                 'encrypted' => true,
                 'host' => '127.0.0.1',
-                'port' => 6001,
+                'port' => env('WS_PORT'),
                 'scheme' => 'http'
             ],
         ],
