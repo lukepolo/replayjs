a20e68a03a9c69ee81e5d0966b76ba90ed2de629
Luke Policinski <luke.policinski@egov.com>
2019-03-27 13:10:26 -0400

    fixing refresh token

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -16,7 +16,7 @@ class AuthController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('auth:api', ['except' => ['login']]);
+        $this->middleware('auth:api', ['except' => ['login', 'refresh']]);
     }
 
     /**
