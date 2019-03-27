3665c9c5add9bcceab1ffb4ff89be8f1796fbfbd
Luke Policinski <luke.policinski@egov.com>
2019-03-27 14:06:03 -0400

    Start listening for events

--- resources/js/views/site/components/mixins/MirrorMixin.js
+++ resources/js/views/site/components/mixins/MirrorMixin.js
@@ -84,44 +84,34 @@ export default {
       this.channel = this.broadcastService.join(
         `stream.${this.$route.params.session}`,
       );
-      // this.channel
-      // .listenForWhisper("initialize", ({ rootId, children, baseHref }) => {
-      //   this.setupMirror(baseHref);
-      //   this.setupIframe({ rootId, children });
-      //   this.channel.whisper("initialized");
-      // })
-      // .listenForWhisper("window-size", ({ width, height }) => {
-      //   this.previewFrame.style.width = width + "px";
-      //   this.previewFrame.style.height = height + "px";
-      //   this.getScale();
-      // })
-      // .listenForWhisper("click", ({ x, y }) => {
-      //   let node = document.createElement("DIV");
-      //   node.style.top = y + "px";
-      //   node.style.left = x + "px";
-      //   this.previewDocument.getElementById("clicks").appendChild(node);
-      //   setTimeout(() => {
-      //     node.remove();
-      //   }, 1001);
-      // })
-      // .listenForWhisper("scroll", ({ scrollPosition }) => {
-      //   window.scrollTo(0, scrollPosition);
-      // })
-      // .listenForWhisper(
-      //   "changes",
-      //   ({ removed, addedOrMoved, attributes, text }) => {
-      //     this.mirror.applyChanged(removed, addedOrMoved, attributes, text);
-      //   },
-      // )
-      // .listenForWhisper("mouse-movement", (movements) => {
-      //   movements.forEach((movement) => {
-      //     setTimeout(() => {
-      //       this.updateCursorPosition(movement.x, movement.y);
-      //     }, movement.timing);
-      //   });
-      //
-      //   this.lastCursorPosition = movements[movements.length - 1];
-      // });
+      this.channel
+        .listenForWhisper("initialize", ({ rootId, children, baseHref }) => {
+          console.info("initialize");
+        })
+        .listenForWhisper("window-size", ({ width, height }) => {
+          console.info("window-size");
+        })
+        .listenForWhisper("click", ({ x, y }) => {
+          console.info("click");
+        })
+        .listenForWhisper("scroll", ({ scrollPosition }) => {
+          console.info("scroll");
+        })
+        .listenForWhisper(
+          "changes",
+          ({ removed, addedOrMoved, attributes, text }) => {
+            console.info("changes");
+          },
+        )
+        .listenForWhisper("mouse-movement", (movements) => {
+          console.info("mouse-movement");
+        })
+        .listenForWhisper("network-request", () => {
+          console.info("network-request");
+        })
+        .listenForWhisper("console-message", () => {
+          console.info("console-message");
+        });
     },
   },
   computed: {},
