c7db8fe8bfa0a0897516f84b32ede0614118fa7b
Luke Policinski <luke.policinski@egov.com>
2019-04-04 15:41:38 -0400

    WIP

--- resources/js/views/site/components/session-player/components/SessionProgressBarCanvas.vue
+++ resources/js/views/site/components/session-player/components/SessionProgressBarCanvas.vue
@@ -50,6 +50,7 @@ export default {
   },
   mounted() {
     this.context = this.canvas.getContext("2d");
+    // This is dictated by the sass
     this.canvas.height = 20;
     this.canvas.style.width = "100%";
     this.canvas.style.height = "20px";
