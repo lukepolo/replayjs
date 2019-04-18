63320526f5e7c064d9814570ff7b8a74eccaa4d1
Luke Policinski <luke.policinski@egov.com>
2019-04-18 11:50:24 -0400

    WIP : removing sone console bugs

--- resources/js/client/mirror/mutation-summary/MutationSummary.ts
+++ resources/js/client/mirror/mutation-summary/MutationSummary.ts
@@ -30,10 +30,6 @@ export default class MutationSummary {
 
   constructor(opts: Options) {
     if (MutationObserver === undefined) {
-      console.error("DOM Mutation Observers are required.");
-      console.error(
-        "https://developer.mozilla.org/en-US/docs/DOM/MutationObserver",
-      );
       throw Error("DOM Mutation Observers are required");
     }
 
