b4757c4a158eeea873c87dac3dd7f55ddc8d3e61
Luke Policinski <luke.policinski@egov.com>
2019-05-15 20:56:19 -0400

    WIP - logo is awful , didnt get very far in like 2 hours of doing shit

--- resources/js/views/layouts/Public.vue
+++ resources/js/views/layouts/Public.vue
@@ -1,14 +1,20 @@
 <template>
   <div id="app">
+    <site-header></site-header>
     <router-view></router-view>
+    <site-footer></site-footer>
   </div>
 </template>
 
 <script>
+import SiteHeader from "./components/SiteHeader";
+import SiteFooter from "./components/SiteFooter";
 import Alerts from "varie/Alerts";
 export default {
   components: {
     Alerts,
+    SiteHeader,
+    SiteFooter,
   },
 };
 </script>
---
+++ resources/js/views/layouts/components/SiteHeader.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="site-header">
+    <div class="site__logo">
+      <replay-logo></replay-logo>
+    </div>
+
+    <div></div>
+
+    <div>
+      <div class="site-header__nav-item">
+        <router-link :to="{ name: 'login' }">Login</router-link>
+      </div>
+      <div class="site-header__nav-item">
+        <router-link :to="{ name: 'register' }">Sign Up</router-link>
+      </div>
+    </div>
+  </div>
+</template>
