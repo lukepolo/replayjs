df857ea32698fae36488a5507c22dd201db699f7
Luke Policinski <luke.policinski@egov.com>
2019-07-15 21:02:21 -0400

    WIP

--- resources/js/app/constants/playerEventTickTypes.ts
+++ resources/js/app/constants/playerEventTickTypes.ts
@@ -1,7 +1,6 @@
 import { playerEventTypes } from "@app/constants/playerEventTypes";
 
 export default [
-  playerEventTypes.DomChange,
   playerEventTypes.MouseClick,
   playerEventTypes.NetworkRequest,
   playerEventTypes.ConsoleMessage,
